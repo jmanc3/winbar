cd04d97a670c55cf4b239791df71a6ec90b82fa0
jmanc3 <jman12316@live.com>
2025-06-23 00:03:29 -0500

    [UI] Reduced label jitter

--- src/taskbar.cpp
+++ src/taskbar.cpp
@@ -584,7 +584,7 @@ paint_icon_label(AppClient *client, cairo_t *cr, Container *container) {
         }
 
         auto [w_f, h_f] = f->begin(ss, EXPAND(config->color_taskbar_button_icons));
-        f->draw_text(5, container->real_bounds.x + 14 * config->dpi + w, MIDY(container) - h_f / 2, data->actual_w - (14 * config->dpi + w));
+        f->draw_text(5, std::round(container->real_bounds.x + 14 * config->dpi + w), MIDY(container) - h_f / 2, data->actual_w - (14 * config->dpi + w));
         f->end();
         
 //        draw_text(client, 9 * config->dpi, config->font, EXPAND(config->color_taskbar_button_icons), data->windows_data_list[0]->title.c_str(), container->real_bounds, -5, 14 * config->dpi + w);
